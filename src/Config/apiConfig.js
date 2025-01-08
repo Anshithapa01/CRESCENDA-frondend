f61deedc4c285aa02e57212cad37b22484ee8430
Anshitha <anshithapa01@gmail.com>
2025-01-08 15:28:06 +0530

    check with local api

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,29 +1,29 @@
-// export const API_BASE_URL = "http://localhost:5454/api"
+export const API_BASE_URL = "http://localhost:5454/api"
 
-// export const MENTOR_BASE_URL="http://localhost:5454/api/mentor"
+export const MENTOR_BASE_URL="http://localhost:5454/api/mentor"
 
-// export const ADMIN_BASE_URL="http://localhost:5454/api/admin"
+export const ADMIN_BASE_URL="http://localhost:5454/api/admin"
 
-// export const QA_BASE_URL="http://localhost:5454/api/qa"
+export const QA_BASE_URL="http://localhost:5454/api/qa"
 
 
 
-// export const API_AUTH_BASE_URL = "http://localhost:5454"
+export const API_AUTH_BASE_URL = "http://localhost:5454"
 
-// export const MENTOR_AUTH_BASE_URL="http://localhost:5454/mentor"
+export const MENTOR_AUTH_BASE_URL="http://localhost:5454/mentor"
 
-// export const ADMIN_AUTH_BASE_URL="http://localhost:5454/admin"
+export const ADMIN_AUTH_BASE_URL="http://localhost:5454/admin"
 
-// export const QA_AUTH_BASE_URL="http://localhost:5454/qa"
+export const QA_AUTH_BASE_URL="http://localhost:5454/qa"
 
 
 // Configuration for API and Auth Base URLs
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const MENTOR_BASE_URL = import.meta.env.VITE_MENTOR_BASE_URL;
-export const ADMIN_BASE_URL = import.meta.env.VITE_ADMIN_BASE_URL;
-export const QA_BASE_URL = import.meta.env.VITE_QA_BASE_URL;
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// export const MENTOR_BASE_URL = import.meta.env.VITE_MENTOR_BASE_URL;
+// export const ADMIN_BASE_URL = import.meta.env.VITE_ADMIN_BASE_URL;
+// export const QA_BASE_URL = import.meta.env.VITE_QA_BASE_URL;
 
-export const API_AUTH_BASE_URL = import.meta.env.VITE_API_AUTH_BASE_URL;
-export const MENTOR_AUTH_BASE_URL = import.meta.env.VITE_MENTOR_AUTH_BASE_URL;
-export const ADMIN_AUTH_BASE_URL = import.meta.env.VITE_ADMIN_AUTH_BASE_URL;
-export const QA_AUTH_BASE_URL = import.meta.env.VITE_QA_AUTH_BASE_URL;
+// export const API_AUTH_BASE_URL = import.meta.env.VITE_API_AUTH_BASE_URL;
+// export const MENTOR_AUTH_BASE_URL = import.meta.env.VITE_MENTOR_AUTH_BASE_URL;
+// export const ADMIN_AUTH_BASE_URL = import.meta.env.VITE_ADMIN_AUTH_BASE_URL;
+// export const QA_AUTH_BASE_URL = import.meta.env.VITE_QA_AUTH_BASE_URL;
