5b5f856817e1d996e0fdb3eafcfd801bf2f946f3
Anshitha <anshithapa01@gmail.com>
2025-01-08 15:49:32 +0530

    config file error solved

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,20 +1,20 @@
-export const API_BASE_URL = "http://localhost:5454/api"
+export const API_BASE_URL = "https://backend.anshitha.cloud/api"
 
-export const MENTOR_BASE_URL="http://localhost:5454/api/mentor"
+export const MENTOR_BASE_URL="https://backend.anshitha.cloud/api/mentor"
 
-export const ADMIN_BASE_URL="http://localhost:5454/api/admin"
+export const ADMIN_BASE_URL="https://backend.anshitha.cloud/api/admin"
 
-export const QA_BASE_URL="http://localhost:5454/api/qa"
+export const QA_BASE_URL="https://backend.anshitha.cloud/api/qa"
 
 
 
-export const API_AUTH_BASE_URL = "http://localhost:5454"
+export const API_AUTH_BASE_URL = "https://backend.anshitha.cloud"
 
-export const MENTOR_AUTH_BASE_URL="http://localhost:5454/mentor"
+export const MENTOR_AUTH_BASE_URL="https://backend.anshitha.cloud/mentor"
 
-export const ADMIN_AUTH_BASE_URL="http://localhost:5454/admin"
+export const ADMIN_AUTH_BASE_URL="https://backend.anshitha.cloud/admin"
 
-export const QA_AUTH_BASE_URL="http://localhost:5454/qa"
+export const QA_AUTH_BASE_URL="https://backend.anshitha.cloud/qa"
 
 
 
