ba199fb4dce729a17a9c95d324d3f00fda462de2
Anshitha <anshithapa01@gmail.com>
2025-01-09 13:33:49 +0530

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   define: {
-//     global: {}, // Define `global` for libraries expecting a Node.js environment
-//   },
-//   server: {
-//     proxy: {
-//       '/ws': {
-//         target: 'http://localhost:5454',
-//         ws: true, // Enable WebSocket proxying
-//       },
-//     },
-//   },
-// });
-
 export default defineConfig({
   plugins: [react()],
   define: {
         global: {}, // Define `global` for libraries expecting a Node.js environment
       },
-  server: {
-    proxy: {
-      '/ws': {
-        target: 'http://localhost:5454',
-        ws: true,
-        changeOrigin: true
-      },
-      '/app': {
-        target: 'http://localhost:5454',
-        changeOrigin: true
-      }
-    }
-  }
 });
