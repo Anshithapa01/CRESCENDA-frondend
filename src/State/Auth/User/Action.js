d0359f9258a59457c15c14907dd692ef532e72b5
Anshitha <anshithapa01@gmail.com>
2025-01-09 16:46:35 +0530

    auth changed

--- src/State/Auth/User/Action.js
+++ src/State/Auth/User/Action.js
@@ -34,11 +34,7 @@ const loginFailure =(error)=>({type:LOGIN_FAILURE,payload:error});
 export const login = (userData) => async(dispatch)=>{
     dispatch(loginRequest())
     try {
-        const response=await axios.post(`${API_AUTH_BASE_URL}/auth/signin`,{headers: {
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-        },withCredentials: true,},
-         userData)
+        const response=await axios.post(`${API_AUTH_BASE_URL}/auth/signin`,userData)
         console.log(response.data);  
         const user=response.data;   
         if(user.jwt){
