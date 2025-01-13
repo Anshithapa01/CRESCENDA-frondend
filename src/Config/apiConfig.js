de9c179f99e11660ef74cc535d4e0a6691c3356d
Anshitha <anshithapa01@gmail.com>
2025-01-13 12:37:43 +0530

    changed to local

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,20 +1,20 @@
-export const API_BASE_URL = "https://backend.anshitha.cloud/api"
+// export const API_BASE_URL = "https://backend.anshitha.cloud/api"
 
-export const MENTOR_BASE_URL="https://backend.anshitha.cloud/api/mentor"
+// export const MENTOR_BASE_URL="https://backend.anshitha.cloud/api/mentor"
 
-export const ADMIN_BASE_URL="https://backend.anshitha.cloud/api/admin"
+// export const ADMIN_BASE_URL="https://backend.anshitha.cloud/api/admin"
 
-export const QA_BASE_URL="https://backend.anshitha.cloud/api/qa"
+// export const QA_BASE_URL="https://backend.anshitha.cloud/api/qa"
 
 
 
-export const API_AUTH_BASE_URL = "https://backend.anshitha.cloud"
+// export const API_AUTH_BASE_URL = "https://backend.anshitha.cloud"
 
-export const MENTOR_AUTH_BASE_URL="https://backend.anshitha.cloud/mentor"
+// export const MENTOR_AUTH_BASE_URL="https://backend.anshitha.cloud/mentor"
 
-export const ADMIN_AUTH_BASE_URL="https://backend.anshitha.cloud/admin"
+// export const ADMIN_AUTH_BASE_URL="https://backend.anshitha.cloud/admin"
 
-export const QA_AUTH_BASE_URL="https://backend.anshitha.cloud/qa"
+// export const QA_AUTH_BASE_URL="https://backend.anshitha.cloud/qa"
 
 
 
@@ -31,22 +31,22 @@ export const QA_AUTH_BASE_URL="https://backend.anshitha.cloud/qa"
 
 
 
-// export const API_BASE_URL = "http://localhost:5454/api"
+export const API_BASE_URL = "http://localhost:5454/api"
 
-// export const MENTOR_BASE_URL="http://localhost:5454/api/mentor"
+export const MENTOR_BASE_URL="http://localhost:5454/api/mentor"
 
-// export const ADMIN_BASE_URL="http://localhost:5454/api/admin"
+export const ADMIN_BASE_URL="http://localhost:5454/api/admin"
 
-// export const QA_BASE_URL="http://localhost:5454/api/qa"
+export const QA_BASE_URL="http://localhost:5454/api/qa"
 
 
 
-// export const API_AUTH_BASE_URL = "http://localhost:5454"
+export const API_AUTH_BASE_URL = "http://localhost:5454"
 
-// export const MENTOR_AUTH_BASE_URL="http://localhost:5454/mentor"
+export const MENTOR_AUTH_BASE_URL="http://localhost:5454/mentor"
 
-// export const ADMIN_AUTH_BASE_URL="http://localhost:5454/admin"
+export const ADMIN_AUTH_BASE_URL="http://localhost:5454/admin"
 
-// export const QA_AUTH_BASE_URL="http://localhost:5454/qa"
+export const QA_AUTH_BASE_URL="http://localhost:5454/qa"
 
 export const GOOGLE_CLIENT_ID = "866856697952-guoojs919a9ue3aqcl9qi1fmdqm1ka4b.apps.googleusercontent.com"
